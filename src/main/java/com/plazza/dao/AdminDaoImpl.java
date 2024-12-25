8d297f6a3625af7c04a002b873809adfc4554c10
Nilkanth2608 <neelgharat143@gmail.com>
2024-12-25 22:03:39 +0530

    login module for customer

--- src/main/java/com/plazza/dao/AdminDaoImpl.java
+++ src/main/java/com/plazza/dao/AdminDaoImpl.java
@@ -5,9 +5,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import com.plazza.pojo.AdiminLogin;
+import com.plazza.pojo.Customer;
 import com.plazza.utility.DBUtility;
 
 public class AdminDaoImpl implements AdminDao {
+	Customer cus = new Customer();
 	Connection con;
 	PreparedStatement ps;
 	String sql;
@@ -19,11 +21,14 @@ public class AdminDaoImpl implements AdminDao {
 	public boolean userLogin(String UEmailid, String Upassword) {
 		try {
 			con=DBUtility.getConnected();
-			sql="Select*from CustomerTb where customerEmailId=? and password_1=?";
+			sql="Select * from CustomerTb where customerEmailId=? and password=?";
 			ps=con.prepareStatement(sql);
 			ps.setString(1, UEmailid);
 			ps.setString(2, Upassword);
+			System.out.println(UEmailid+ ","+ Upassword);
 			rs=ps.executeQuery();
+			
+			System.out.println("Customer Name :" + cus.getCustomerName());
 			if(rs.next())
 			{
 				return true;
@@ -43,7 +48,7 @@ public class AdminDaoImpl implements AdminDao {
 	public boolean userChangePassword(String UEmailid, String newUpassword) {
 		try {
 			con=DBUtility.getConnected();
-			sql="Update CustomerTb set password_1=? where customerEmailId=?";
+			sql="Update CustomerTb set password=? where customerEmailId=?";
 			ps=con.prepareStatement(sql);
 			ps.setString(1, newUpassword);
 			ps.setString(2, UEmailid);
@@ -67,7 +72,7 @@ public class AdminDaoImpl implements AdminDao {
 	public boolean AdminLogin(String Emailid, String password) {
 		try {
 			con=DBUtility.getConnected();
-			sql="select*from AdminLoginTb where AdminEmailId=? and password_1=?";
+			sql="select * from AdminLoginTb where AdminEmailId=? and password=?";
 			ps=con.prepareStatement(sql);
 			ps.setString(1, Emailid);
 			ps.setString(2, password);
